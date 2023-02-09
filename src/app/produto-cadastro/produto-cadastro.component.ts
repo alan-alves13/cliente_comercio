75546867a5ba98322cdce4a2f113bfa756d51e22
alan <alan.s@escolar.ifrn.edu.br>
2023-02-09 18:35:06 -0300

    ajuste no salvar - produto

--- src/app/produto-cadastro/produto-cadastro.component.ts
+++ src/app/produto-cadastro/produto-cadastro.component.ts
@@ -1,99 +1,3 @@
-/*
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
-import { ServicoService } from '../servico.service';
-import { Router } from '@angular/router';
-import { Produto } from '../produtos';
-
-
-@Component({
-  selector: 'app-produto-cadastro',
-  templateUrl: './produto-cadastro.component.html',
-  styleUrls: ['./produto-cadastro.component.css']
-})
-export class ProdutoCadastroComponent implements OnInit {
-  
-  id?: number;
-  isNew = true;
-  fileName = '';
-  produto?: Produto;
-  selectedFile!: File;
-  
-  crudForm: FormGroup = this.formBuilder.group({
-    id: [0],
-    nome: ['', Validators.required],
-    preco: [0.0, Validators.required],
-    quantidade_estoque: [0, Validators.required],
-    descricao: ['', Validators.required],
-    categoria: [0, Validators.required],
-    foto: ['', Validators.required]
-   
-  })
-  
-  constructor(private route: ActivatedRoute, private formBuilder: FormBuilder,
-    private service: ServicoService, private router: Router){}
-    
-  onFileSelected(event: any) {
-      this.selectedFile = <File>event.target.files[0]
-      console.log(event)
-  }
-  
-
-  ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id') ? parseInt(this.route.snapshot.paramMap.get('id')!):0;
-
-    if(this.id>0){
-      this.isNew = false;
-      this.service.getProdutoByID(this.id).subscribe({
-       next: (produto: Produto) => this.crudForm.setValue(produto),
-       error: (erro: any) => console.log(erro),
-       complete: () => console.log('finalizado')
-      });
-    } else {
-      this.isNew = true; 
-    }
-  }
-
-  
-  salvar(): void {
-    
-
-    if(this.isNew){
-      this.service.addProduto(this.crudForm.value).subscribe({
-        next: (produto: Produto) =>{
-          console.log(produto);
-          this.router.navigate(['/produtoslistagem']);
-        },
-        error: (erro: any) => console.log(erro),
-        complete: () => console.log('Finalizado')
-      });
-    } else {
-      this.service.updateProduto(this.crudForm.value).subscribe({
-        next: (produto: Produto) => {
-            console.log(produto);
-            this.router.navigate(['/produtoslistagem']);
-          },
-          error: (erro: any) => console.log(erro),
-          complete: () => console.log('Finalizado')
-      });
-    }
-  }
-
-  deletar(): void {
-    if (!this.isNew){
-      this.service.detelarProduto(this.id!).subscribe({
-        next: (produto: Produto) =>{
-          console.log(produto);
-          this.router.navigate(['/produtoslistagem'])
-        },
-        error: (erro: any) => console.log(erro),
-        complete: () => console.log('Finalizado')
-      });
-    }
-  }
-}
-*/
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -160,8 +64,12 @@ export class ProdutoCadastroComponent implements OnInit {
      
      formData.append('categoria', this.crudForm.get('categoria')?.value);
 
-     this.httpClient.post<any>(this.server_url_produto, formData).subscribe(
-            //o erro e response
+     this.httpClient.post(this.server_url_produto, formData).subscribe(
+           res => {
+            console.log(res);
+            alert('foto salva com sucesso');
+            this.router.navigate(['/produtoslistagem']);
+           }
      )
    
     next: (produto: Produto) =>{
